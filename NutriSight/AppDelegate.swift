66a805de5ac15b2a0bb1a4d67d879849a72f31fc
Swarasai Mulagari <125925496+swarasai@users.noreply.github.com>
2025-03-22 15:43:57 -0700

    Add files via upload

---
+++ NutriSight/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  NutriSight
+//
+//  Created by Swarasai Mulagari on 3/22/25.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
